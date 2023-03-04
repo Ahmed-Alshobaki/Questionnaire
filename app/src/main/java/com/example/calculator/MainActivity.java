3ee08f30a2c24ceda8bd89f10b930cb70c3c8598
Ahmed Alshobaki <117390882+Ahmed-Alshobaki@users.noreply.github.com>
2023-03-04 18:38:37 +0200

    Create a form to register people in a programming course, the form must contains the following: a- Person name b- Person age, ages categories: from 15 to 20 from 21 to 4 from 41 to 60 c- Person gender d- Programming languages which he/she know ,(don't use EditText)
    e. Save button: when you click on it you have to change the background color of the root layout,
    also you should view on TexView the age stage name as:
    young (from 15 to 20)
     youths (from 21 to 40)
    older (from 41 to 60

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText edit_expiry_date =findViewById(R.id.edit_expiry_date);
+        RadioGroup Rog   = findViewById(R.id.radioGroup);
+        Button   button = findViewById(R.id.button);
+        RadioButton radioButton1 =findViewById(R.id.radioButton); RadioButton radioButton2 =findViewById(R.id.radioButton2);RadioButton radioButton3 =findViewById(R.id.radioButton3);
+        TextView seen = findViewById(R.id.seen);TextView Tx = findViewById(R.id.textView);
+        ConstraintLayout per  =findViewById(R.id.pearnt);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioButton1.isChecked()){
+                    Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                    seen.setText("young");
+                } else if (radioButton2.isChecked()){
+
+                    Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                    seen.setText("youths");
+                }
+                else if (radioButton3.isChecked()){
+
+                    Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                    seen.setText("older");
+                }
+                    per.setBackgroundResource(R.color.no);
+                Tx.setTextColor(getColor(R.color.black));
+
+               
+            }
+        });
+
+    }
+}
